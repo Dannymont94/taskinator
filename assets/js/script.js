62ebf26802cc7cbc56f0c7b4a819a67d08a57b26
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-06-23 12:45:31 -0400

    defined createTaskHandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,24 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(){
+    // prevent refresh on submit button click
     event.preventDefault();
+    // get values from form
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // add HTML content to div created by taskInfoEl
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // add div to li element created by listItemEl
+    listItemEl.appendChild(taskInfoEl);
+    //add list item to tasksToDoEl
     tasksToDoEl.appendChild(listItemEl);
-}
+    console.dir(listItemEl);
+};
 
 formEl.addEventListener("submit", createTaskHandler);
