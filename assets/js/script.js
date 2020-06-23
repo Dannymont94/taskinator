9e1ec69ec15bd0177e1bdc2d93ff672d7c44bf6a
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-06-23 17:02:26 -0400

    added buttons to list items and created delete button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var pageContentEl = document.querySelector("#page-content");
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -12,7 +14,7 @@ var taskFormHandler = function(){
         alert("You need to fill out the task form!");
         return false;
     }
-    // reset form to blank values
+    // reset form to default after submitting task
     formEl.reset();
     // package up data as an object
     var taskDataObj = {
@@ -24,18 +26,69 @@ var taskFormHandler = function(){
 };
 
 var createTaskEl = function(taskDataObj){
-    // create list item
+    // create list item 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    // create div to hold task info and add to list item
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+    // create div to hold task info and then add to list item created by listItemEl
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    // add HTML content to div created by taskInfoEl
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-    // add div to li element created by listItemEl
     listItemEl.appendChild(taskInfoEl);
+    // create edit button, delete button, and status dropdown and then add to list created by listItemEl
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    listItemEl.appendChild(taskActionsEl);
     //add list item to tasksToDoEl
     tasksToDoEl.appendChild(listItemEl);
+    // increase task counter for next unique id
+    taskIdCounter++;
+};
+
+var createTaskActions = function(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+    // create edit button and add to div created by actionContainerEl
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+    actionContainerEl.appendChild(editButtonEl);
+    // create delete button and add to div created by actionContainerEl
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+    actionContainerEl.appendChild(deleteButtonEl);
+    //create dropdown menu and add to div created by actionContainerEl
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+    actionContainerEl.appendChild(statusSelectEl);
+    // create option elements and add to select created by statusSelectEl
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+    for (i = 0; i < statusChoices.length; i++) {
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+    // return div containing edit button, delete button, and dropdown menu
+    return actionContainerEl;
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler)
